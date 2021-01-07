8b0f5e37c93b5f660b7859e95c1052cd0c1b3ec7
tris84 <jollytris@gmail.com>
2021-01-07 23:45:39 +0900

    insert test

--- app/src/main/java/com/tr1984/smilecompetition/CalendarViewModel.kt
+++ app/src/main/java/com/tr1984/smilecompetition/CalendarViewModel.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.viewModelScope
 import com.tr1984.smilecompetition.data.BePrettyDatabase
 import com.tr1984.smilecompetition.data.Smiling
 import kotlinx.coroutines.launch
+import java.util.*
 
 class CalendarViewModel(private val db: BePrettyDatabase) : ViewModel() {
 
@@ -21,6 +22,22 @@ class CalendarViewModel(private val db: BePrettyDatabase) : ViewModel() {
         viewModelScope.launch {
             val histories = db.dao().getAll()
             Log.d("1984tr", "histories: ${histories.size}")
+            if (histories.isEmpty()) {
+                val cal = Calendar.getInstance()
+                val yyyy = cal.get(Calendar.YEAR)
+                val mm = cal.get(Calendar.MONTH)
+                val dd = cal.get(Calendar.DATE)
+                cal.set(yyyy, mm, dd, 0, 0, 0)
+
+                val smiling = Smiling(false, cal.time)
+                db.dao().insert(smiling)
+                _histories.value = listOf(smiling)
+            } else {
+                histories.forEach {
+                    Log.d("1984tr", it.toString())
+                }
+                _histories.value = histories
+            }
         }
     }
 }
--- app/src/main/java/com/tr1984/smilecompetition/data/Smiling.kt
+++ app/src/main/java/com/tr1984/smilecompetition/data/Smiling.kt
@@ -1,14 +1,17 @@
 package com.tr1984.smilecompetition.data
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import java.text.SimpleDateFormat
 import java.util.*
 
-@Entity(tableName = "smiling")
+@Entity(tableName = "smiling", indices = [Index(value = ["createdAt"], unique = true)])
 data class Smiling(
-    @PrimaryKey(autoGenerate = true) val id: Long, val checkIn: Boolean, val createdAt: Date
+    @PrimaryKey(autoGenerate = true) val id: Long?, val checkIn: Boolean, val createdAt: Date
 ) {
+    constructor(checkIn: Boolean, createdAt: Date) : this(null, checkIn, createdAt)
+
     fun date() : String {
         return SimpleDateFormat("MM/dd E").format(createdAt)
     }
