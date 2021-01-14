226ff2794403ff0fda2e1f1e262687103aeb9517
tris84 <jollytris@gmail.com>
2021-01-14 22:47:33 +0900

    proto datastore -> preference datastore

--- app/src/main/java/com/tr1984/smilecompetition/PreviewActivity.kt
+++ app/src/main/java/com/tr1984/smilecompetition/PreviewActivity.kt
@@ -4,15 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.util.Size
-import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.preferencesKey
+import androidx.datastore.preferences.createDataStore
+import androidx.lifecycle.lifecycleScope
 import com.tr1984.smilecompetition.databinding.ActivityPreviewBinding
 import com.tr1984.smilecompetition.util.ImageProcessor
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import kotlin.math.min
 
 class PreviewActivity : AppCompatActivity() {
@@ -20,8 +26,8 @@ class PreviewActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPreviewBinding
     private val cameraSelector by lazy {
         CameraSelector.Builder()
-                .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
-                .build()
+            .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
+            .build()
     }
     private val targetResolution = Size(480, 480) // 인식 가능한 최소 크기는 480 x 360 이다.
     private val preview by lazy {
@@ -31,17 +37,20 @@ class PreviewActivity : AppCompatActivity() {
     private var imageProcessor: ImageProcessor? = null
     private var startTimestamp = Long.MAX_VALUE
 
+    private val dataStore = createDataStore("smile_config")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ActivityPreviewBinding.inflate(layoutInflater)
-                .apply {
-                    closeBtn.setOnClickListener { finish() }
-                    calendarBtn.setOnClickListener { moveToCalendar(false) }
-                }.also {
-                    binding = it
-                }.run {
-                    setContentView(root)
-                }
+            .apply {
+                closeBtn.setOnClickListener { finish() }
+                calendarBtn.setOnClickListener { moveToCalendar(false) }
+                timerBtn.setOnClickListener { showTimerDialog() }
+            }.also {
+                binding = it
+            }.run {
+                setContentView(root)
+            }
 
         val providerFuture = ProcessCameraProvider.getInstance(this)
         providerFuture.addListener({
@@ -50,6 +59,14 @@ class PreviewActivity : AppCompatActivity() {
         }, ContextCompat.getMainExecutor(this))
 
         imageProcessor = ImageProcessor(this)
+
+        lifecycleScope.launch {
+            val preferences = dataStore.data.first()
+            val key = preferencesKey<Long>("timer")
+            val timer = preferences[key] ?: 10000L
+            Log.d("1984tr", "get timer: $timer")
+            binding.progress.max = timer.toInt()
+        }
     }
 
     override fun onResume() {
@@ -77,8 +94,8 @@ class PreviewActivity : AppCompatActivity() {
 
 
         val imageAnalysis = ImageAnalysis.Builder()
-                .setTargetResolution(targetResolution)
-                .build()
+            .setTargetResolution(targetResolution)
+            .build()
 
         imageProcessor?.stop()
         imageProcessor = ImageProcessor(this)
@@ -89,7 +106,10 @@ class PreviewActivity : AppCompatActivity() {
                     if (startTimestamp == Long.MAX_VALUE) {
                         startTimestamp = System.currentTimeMillis()
                     } else {
-                        val progress = min((System.currentTimeMillis() - startTimestamp).toInt(), binding.progress.max)
+                        val progress = min(
+                            (System.currentTimeMillis() - startTimestamp).toInt(),
+                            binding.progress.max
+                        )
                         binding.progress.progress = progress
                         if (progress >= binding.progress.max) {
                             moveToCalendar(true)
@@ -110,5 +130,31 @@ class PreviewActivity : AppCompatActivity() {
         startActivity(Intent(this, CalendarActivity::class.java).apply {
             putExtra("withInsert", withInsert)
         })
+    }
+
+    private fun showTimerDialog() {
+        AlertDialog.Builder(this)
+            .setItems(timers.map { it.first }.toTypedArray()) { dialogInterface, position ->
+                val timer = timers[position].second
+                Log.d("1984tr", "set timer: $timer")
+                lifecycleScope.launch {
+                    dataStore.edit {
+                        val key = preferencesKey<Long>("timer")
+                        it[key] = timer.toLong()
+                    }
+                    binding.progress.max = timer
+
+                }
+            }.create().show()
+    }
+
+    companion object {
+        val timers = listOf(
+            "5s" to 5 * 1000,
+            "7s" to 7 * 1000,
+            "10s" to 10 * 1000,
+            "15s" to 15 * 1000,
+            "20s" to 20 * 1000
+        )
     }
 }
