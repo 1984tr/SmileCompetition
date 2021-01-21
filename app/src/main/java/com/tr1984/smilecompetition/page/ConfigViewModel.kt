691f8b5c55c62981d86f1159146f4ee3f91e54f9
tris84 <jollytris@gmail.com>
2021-01-22 00:30:16 +0900

    regist alarm

--- app/src/main/java/com/tr1984/smilecompetition/page/ConfigViewModel.kt
+++ app/src/main/java/com/tr1984/smilecompetition/page/ConfigViewModel.kt
@@ -8,10 +8,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.tr1984.smilecompetition.util.AlarmHelper
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
-class ConfigViewModel(private val dataStore: DataStore<Preferences>) : ViewModel() {
+class ConfigViewModel(private val dataStore: DataStore<Preferences>, private val alarmHelper: AlarmHelper) : ViewModel() {
 
     private val _duration = MutableLiveData(15)
     val duration: LiveData<Int>
@@ -55,16 +56,19 @@ class ConfigViewModel(private val dataStore: DataStore<Preferences>) : ViewModel
     fun changeHour(hour: Int) {
         _hour.value = hour
         saveDataStore("hour", hour)
+        regist()
     }
 
     fun changeMinute(minute: Int) {
         _minute.value = minute
         saveDataStore("minute", minute)
+        regist()
     }
 
     fun changeAmpm(ampm: String) {
         _ampm.value = ampm
-        saveDataStore("ampm", ampm)
+        saveDataStore("ampm", ampm.toLowerCase())
+        regist()
     }
 
     private fun saveDataStore(key: String, value: Any) {
@@ -73,5 +77,12 @@ class ConfigViewModel(private val dataStore: DataStore<Preferences>) : ViewModel
                 it[preferencesKey(key)] = value
             }
         }
+    }
+
+    private fun regist() {
+        val isAm = _ampm.value == "AM".toLowerCase()
+        val hour = (_hour.value ?: 10) + (if (isAm) 0 else 12)
+        val min = _minute.value ?: 10
+        alarmHelper.regist(hour, min)
     }
 }
