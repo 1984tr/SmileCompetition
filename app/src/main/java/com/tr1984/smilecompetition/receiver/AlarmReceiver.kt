c34ec871028fbb974a62ed8acef24fd2515d5ce3
tris84 <jollytris@gmail.com>
2021-02-05 23:27:54 +0900

    add reciever

---
+++ app/src/main/java/com/tr1984/smilecompetition/receiver/AlarmReceiver.kt
@@ -0,0 +1,12 @@
+package com.tr1984.smilecompetition.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+    }
+}
